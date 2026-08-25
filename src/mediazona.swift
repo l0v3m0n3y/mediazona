ba913df8fce4f011eaf9b63a118c8288d5ac24b3
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:39:29 +0300

    Update mediazona.swift

--- src/mediazona.swift
+++ src/mediazona.swift
@@ -20,6 +20,14 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
+
 public class Mediazona{
     private let apiShares = "https://zona.media/_shares"
     private let api = "https://zona.media/api"
